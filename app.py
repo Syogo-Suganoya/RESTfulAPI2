d02c903f41eae24af4f82bf1505dd018bdaaec96
Syogo-Suganoya <registration@ssuganoya.com>
2025-04-21 12:54:39 +0900

    add sqlite

--- app.py
+++ app.py
@@ -1,13 +1,63 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from models import db, Todo
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 
-@app.route("/")
-def hello():
-    return jsonify(message="Hello from Flask on Render!")
+@app.before_first_request
+def create_tables_and_seed():
+    db.create_all()
+    if not Todo.query.first():  # データが空のときだけ
+        sample_todos = [
+            Todo(title="買い物に行く", done=False),
+            Todo(title="読書をする", done=False),
+            Todo(title="コードを書く", done=True)
+        ]
+        db.session.bulk_save_objects(sample_todos)
+        db.session.commit()
 
 
-# デバッグモードで動かさないよう注意
-if __name__ == "__main__":
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    todos = Todo.query.all()
+    return jsonify([todo.to_dict() for todo in todos])
+
+
+@app.route('/todos/<int:todo_id>', methods=['GET'])
+def get_todo(todo_id):
+    todo = Todo.query.get_or_404(todo_id)
+    return jsonify(todo.to_dict())
+
+
+@app.route('/todos', methods=['POST'])
+def create_todo():
+    data = request.get_json()
+    todo = Todo(title=data['title'], done=False)
+    db.session.add(todo)
+    db.session.commit()
+    return jsonify(todo.to_dict()), 201
+
+
+@app.route('/todos/<int:todo_id>', methods=['PUT'])
+def update_todo(todo_id):
+    todo = Todo.query.get_or_404(todo_id)
+    data = request.get_json()
+    todo.title = data.get('title', todo.title)
+    todo.done = data.get('done', todo.done)
+    db.session.commit()
+    return jsonify(todo.to_dict())
+
+
+@app.route('/todos/<int:todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    todo = Todo.query.get_or_404(todo_id)
+    db.session.delete(todo)
+    db.session.commit()
+    return '', 204
+
+
+if __name__ == '__main__':
     app.run()
